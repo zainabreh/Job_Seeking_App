74d66e46b5e9ccf73cbf0b18d659f163128dbf22
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-08-21 23:41:11 +0500

    Authentication and Authorization done

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,64 @@
-export const signup = (req,res,next)=>{
-    
+import userModel from "../model/user.model.js"
+import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken';
+
+
+export const signup = async (req,res,next)=>{
+    let user = req.body
+    try {
+        console.log(user);
+        let userPassword = user.password
+        user.password = await bcrypt.hash(userPassword,10)
+        
+        await userModel.create(user)
+        res.json({
+            message: "User signUp"
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
-export const login = (req,res,next)=>{
+export const login = async (req,res,next)=>{
+    let user = req.body
 
+    try {
+
+        if(!user.email) return next(new Error("provide Email"))
+        if(!user.password) return next(new Error("provide Password"))
+
+
+        let dbuser = await userModel.findOne({email:user.email})
+
+        if(dbuser === null) return next(new Error("Invalid User"))
+
+        let logpassword = await bcrypt.compare(user.password,dbuser.password)
+        
+        if(!logpassword) return next(new Error("Invalid Password"))
+
+        let jwt_key = jwt.sign({
+            id:dbuser._id,
+            username:dbuser.username,
+            email:dbuser.email,
+            role:dbuser.roles
+        },process.env.TOKEN,{expiresIn: '2h'})
+
+        res.cookie("auth",jwt_key,{maxAge:90000,httpOnly:true}).json({
+            token:jwt_key,
+            message:"logIn successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const logout = (req,res,next)=>{
 
+    try {
+        res.cookie("auth",null,{maxAge: Date.now()}).json({
+            message:"logout successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
 }
--- backend/script.js
+++ backend/script.js
@@ -4,9 +4,14 @@ import { connectDB } from './config/connectdb.js'
 import userRouter from "./routing/user.routing.js"
 import authRouter from "./routing/auth.routing.js"
 import { errorHandler } from './middleware/error.middleware.js'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 connectDB()
 const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.use("/",userRouter)
 app.use("/",authRouter)
