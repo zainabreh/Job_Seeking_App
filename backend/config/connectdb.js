e265e2103b764673991b1ceff086209b18d29de4
zainabreh <zainab.rd.93@gmail.com>
2024-11-16 20:57:10 -0800

    changing db connection to cloud db connection

--- backend/config/connectdb.js
+++ backend/config/connectdb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async ()=>{
     try {
-        const db = await mongoose.connect('mongodb://localhost:27017/job-finding-app')
+        const db = await mongoose.connect('mongodb+srv://zainabrd93:O1VL0tXW8lYznEhr@cluster0.rep2s.mongodb.net/');
         console.log("connection was successfully");        
     } catch (error) {
         console.log(error);
